cf6567f1a75a075b9bfe5bb6014ce866b6871263
Mariia_Cydeo <mashkenciy@gmail.com>
2023-03-01 15:19:16 -0500

    updated BasePage with my profile options and method

--- src/test/java/com/crm/pages/BasePage.java
+++ src/test/java/com/crm/pages/BasePage.java
@@ -42,61 +42,34 @@ public abstract class BasePage {
     }
 
 
-    /**
-     * @return page name, for example: Dashboard
-     */
-    public String getPageSubTitle() {
-        //ant time we are verifying page name, or page subtitle, loader mask appears
-        waitUntilLoaderScreenDisappear();
-//        BrowserUtils.waitForStaleElement(pageSubTitle);
-        return pageSubTitle.getText();
-    }
+    @FindBy(css = ".user-img.user-default-avatar")
+    public WebElement myProfileIcon;
 
 
-    /**
-     * Waits until loader screen present. If loader screen will not pop up at all,
-     * NoSuchElementException will be handled  bu try/catch block
-     * Thus, we can continue in any case.
-     */
-    public void waitUntilLoaderScreenDisappear() {
-        try {
-            WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
-            wait.until(ExpectedConditions.invisibilityOf(loaderMask));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    @FindBy(xpath = "//span[.='My Profile']")
+    public WebElement myProfileOption;
 
-    }
+    @FindBy(xpath = "//span[.='Edit Profile Settings']")
+    public WebElement editProfileSettingsOption;
 
+    @FindBy(xpath = "//span[@class='menu-popup-item menu-popup-no-icon ']//span[.='Themes']")
+    public WebElement themesOption;
 
+    @FindBy(xpath = "(//span[.='Configure notifications'])[2]")
+    public WebElement configureNotificationsOption;
+
+    @FindBy(xpath = "//span[.='Log out']")
+    public WebElement logoutOption;
 
     /**
-     * This method will navigate user to the specific module in vytrack application.
-     * For example: if tab is equals to Activities, and module equals to Calls,
-     * Then method will navigate user to this page: http://qa2.vytrack.com/call/
-     *
-     * @param tab
-     * @param module
+     * Method that will allow to access options under my profile
+     * @param option
      */
-    public void navigateToModule(String tab, String module) {
-        String tabLocator = "//span[normalize-space()='" + tab + "' and contains(@class, 'title title-level-1')]";
-        String moduleLocator = "//span[normalize-space()='" + module + "' and contains(@class, 'title title-level-2')]";
-        try {
-            BrowserUtils.waitForClickablility(By.xpath(tabLocator), 5);
-            WebElement tabElement = Driver.getDriver().findElement(By.xpath(tabLocator));
-            new Actions(Driver.getDriver()).moveToElement(tabElement).pause(200).doubleClick(tabElement).build().perform();
-        } catch (Exception e) {
-            BrowserUtils.clickWithWait(By.xpath(tabLocator), 5);
-        }
-        try {
-            BrowserUtils.waitForPresenceOfElement(By.xpath(moduleLocator), 5);
-            BrowserUtils.waitForVisibility(By.xpath(moduleLocator), 5);
-            BrowserUtils.scrollToElement(Driver.getDriver().findElement(By.xpath(moduleLocator)));
-            Driver.getDriver().findElement(By.xpath(moduleLocator)).click();
-        } catch (Exception e) {
-//            BrowserUtils.waitForStaleElement(Driver.get().findElement(By.xpath(moduleLocator)));
-            BrowserUtils.clickWithTimeOut(Driver.getDriver().findElement(By.xpath(moduleLocator)),  5);
-        }
+    public void selectOptionUnderProfile(String option){
+        myProfileIcon.click();
+        String locator = "//div[@class='menu-popup-items']//span[contains(.,'"+option+"')]";
+        Driver.getDriver().findElement(By.xpath(locator)).click();
     }
+
 
 }
